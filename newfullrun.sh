37962041e817a9969128e71e14e17e95861fd083
Zenovak <xe.novak@outlook.com>
2025-07-28 13:55:53 +0800

    refactor: moved frontend material into frontend dir
    - updated .sh launch file

--- newfullrun.sh
+++ newfullrun.sh
@@ -1,3 +1,3 @@
 screen -dmS whisper bash -c 'source "bin/activate" && python3 backend/whisper_streaming/transcribe.py --model tiny.en; exec sh' 
 
-screen -dmS fe bash -c 'npm run build && node server/server.js; exec sh' 
+screen -dmS fe bash -c 'cd frontend && npm run build && node server/server.js; exec sh' 
